2912e0be3731c35dec4735057bd0417a4b9d4f79
Julien DOCK <dock.julien@orange.fr>
2021-11-14 10:04:40 +0100

    bug correct collector

--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Collector : MonoBehaviour
 {
     public static int totalCollected = 0;
-
+    private SphereCollider collider;
+    /*
     private void OnTriggerStay(Collider other)
     {
         Collectible collectible = other.gameObject.GetComponent<Collectible>();
@@ -25,6 +26,45 @@ public class Collector : MonoBehaviour
             }
         }
     }
+    */
+
+    private void Start()
+    {
+        collider = GetComponent<SphereCollider>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Collect"))
+        {
+            Collider[] contactList = Physics.OverlapSphere(collider.bounds.center, collider.radius);
+
+            foreach (var contact in contactList)
+            {
+                if (contact.gameObject.CompareTag("Collectible"))
+                {
+                    Collectible collectible = contact.gameObject.GetComponent<Collectible>();
+                    if (!collectible.isCollected())
+                    {
+                        collectible.SetCollected();
+                        CollectibleDataManager dataManager = contact.gameObject.GetComponent<CollectibleDataManager>();
+                        CollectibleData data = dataManager.GetData();
+
+                        IncrementCount();
+                        LocalTabletManager.collectibleDataList.Add(data);
+                        LocalTabletManager.reorderLogList();
+
+                        contact.gameObject.SetActive(false);
+                        
+                    }
+                    
+
+
+                }
+            }
+        }
+    }
+
 
     private static void IncrementCount()
     {
