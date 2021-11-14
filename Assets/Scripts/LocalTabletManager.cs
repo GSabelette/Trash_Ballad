94fa3a540c71d70ef989ca33da4eb0772eff95f2
GSabelette <32684926+GSabelette@users.noreply.github.com>
2021-11-14 02:42:46 +0100

    logs display scalable

--- Assets/Scripts/LocalTabletManager.cs
+++ Assets/Scripts/LocalTabletManager.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
 using TMPro;
+using System;
 public class LocalTabletManager : MonoBehaviour
 {
+    private int LINE_LENGTH = 37;
+
     public Image image;
     public TextMeshProUGUI logText;
     [SerializeField] public Sprite spriteLogs;
@@ -31,24 +34,64 @@ public class LocalTabletManager : MonoBehaviour
         collectibleDataList.Sort((data1, data2) => data1.year.CompareTo(data2.year));
     }
 
+    private String partialDisplayLog(int logIndex)
+    {
+        CollectibleData curData = collectibleDataList[logIndex];
+        return ("Log " + (logIndex).ToString() + " | year " + curData.year + "\n");
+    }
+    private String fullDisplayLog(int curLogIndex)
+    {
+        // Middle log
+        // All the 17 below are = LINE_LENGTH - 20
+        String localString = "";
+        CollectibleData curData = collectibleDataList[curLogIndex];
+        int nbLines = (curData.description.Length + 20) / LINE_LENGTH;
+        localString += "Log " + curLogIndex.ToString() + " | year " + curData.year + " : " + curData.description.Substring(0, LINE_LENGTH - 20) + "\n";
+
+        for (int j = 1; j < nbLines; j++)
+        {
+            localString += curData.description.Substring(17 + (j - 1) * LINE_LENGTH, LINE_LENGTH) + "\n";
+        }
+
+        localString += curData.description.Substring(17 + (nbLines - 1) * LINE_LENGTH, curData.description.Length - (17 + (nbLines - 1) * LINE_LENGTH)) + "\n";
+        return localString;
+    }
+
     public void changeLogText(int curLogIndex)
     {
         string totalString = "";
+
         if (collectibleDataList.Count != 0)
         {
-            for (int i = 0; i < Collector.totalCollected; i++)
+            // Limit case lower bound
+            if (curLogIndex == 0)
             {
-                CollectibleData curData = collectibleDataList[i];
-                if (i != curLogIndex)
+                totalString += fullDisplayLog(curLogIndex);
+                if (collectibleDataList.Count >= 2)
                 {
-                    totalString += "Log " + i.ToString() + " | year " + curData.year + "\n";
+                    totalString += partialDisplayLog(curLogIndex + 1);
                 }
-                if (i == curLogIndex)
-                {
-                    totalString += "Log " + i.ToString() + " | year " + curData.year + " : " + curData.description + "\n";
-                }
-                
             }
+            // Limit case upper bound
+            else if (curLogIndex == Collector.totalCollected - 1) 
+            {
+                if (collectibleDataList.Count >= 2)
+                {
+                    totalString += partialDisplayLog(curLogIndex - 1);
+                }
+                totalString += fullDisplayLog(curLogIndex);
+            }
+            // Normal case
+            else
+            {
+                // Upper log
+                totalString += partialDisplayLog(curLogIndex - 1);
+                // Main log
+                totalString += fullDisplayLog(curLogIndex);
+                // Lower log
+                totalString += partialDisplayLog(curLogIndex + 1);
+            }
+
             logText.text = totalString;
         }
     }
